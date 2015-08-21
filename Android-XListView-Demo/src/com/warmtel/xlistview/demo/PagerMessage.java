7e44936e9da046240bf9d38e02e8b7be2943cae2
viktor.zhou <2915858910@qq.com>
2015-08-21 15:17:37 +0800

    first commit

---
+++ Android-XListView-Demo/src/com/warmtel/xlistview/demo/PagerMessage.java
@@ -0,0 +1,24 @@
+package com.warmtel.xlistview.demo;
+
+import java.util.List;
+
+public class PagerMessage {
+	private List<String> listData;
+	private int pageCount;
+
+	public List<String> getListData() {
+		return listData;
+	}
+
+	public void setListData(List<String> listData) {
+		this.listData = listData;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+}
---
+++ Android-XListView-library/gen/com/example/android_xlistview_library/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.android_xlistview_library;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
